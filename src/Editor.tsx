6adf29ab10cfebbe8bb644eaa53f7c11c06a6cc7
Elad Cohen <ecohen@cloudbees.com>
2023-03-05 14:44:07 +0200

    init

---
+++ src/Editor.tsx
@@ -0,0 +1,239 @@
+
+import 'monaco-editor/esm/vs/editor/editor.all.js';
+
+// support all editor features
+import 'monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch.js';
+import 'monaco-editor/esm/vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast.js';
+
+import * as monaco from 'monaco-editor';
+
+import { buildWorkerDefinition } from 'monaco-editor-workers';
+
+import { CloseAction, ErrorAction, MessageTransports, MonacoLanguageClient, MonacoServices } from 'monaco-languageclient';
+import { BrowserMessageReader, BrowserMessageWriter } from 'vscode-languageserver-protocol/browser.js';
+import { IGrammarDefinition, Registry } from 'monaco-textmate';
+import { wireTmGrammars } from 'monaco-editor-textmate';
+import Editor, { loader } from "@monaco-editor/react";
+import { StandaloneServices } from 'vscode/services';
+import getMessageServiceOverride from 'vscode/service-override/messages';
+import React, { createRef, useEffect, useState, useRef } from 'react';
+import { WebContainer } from '@webcontainer/api';
+import darkPlusTMTheme from './monaco-themes/dark_plus.js';
+import convertTheme from './monaco-themes/convert-tmtheme.js';
+import { debounce } from 'lodash';
+
+import wingJson from './grammers/wing.tmLanguage.json'
+import LspWorker from './lsp.js?worker'
+import files from './files';
+import { initContainer, installDependencies, compile, test, prepareForEvaluation } from './containers';
+
+
+const darkPlusTheme = convertTheme(darkPlusTMTheme);
+
+loader.config({ monaco });
+
+StandaloneServices.initialize({
+    ...getMessageServiceOverride(document.body)
+});
+buildWorkerDefinition('dist', new URL('', window.location.href).href, false);
+
+export type EditorProps = {
+    defaultCode?: string;
+    hostname?: string;
+    port?: string;
+    path?: string;
+    className?: string;
+}
+
+
+
+async function wireGrammers(monaco: any) {
+  const registry = new Registry({
+    getGrammarDefinition: async (scopeName: string, dependantScope: string): Promise<IGrammarDefinition> => {
+      return {
+        format: 'json',
+        content: wingJson
+      };
+    }
+  });
+
+  const grammers = new Map();
+
+  grammers.set('wing', 'source.wing');
+
+  return wireTmGrammars(monaco, registry, grammers);
+}
+
+enum LoadingStatus {
+  Init = "Initializing WebContainer...",
+  Install = "Installing dependencies...",
+  Eval = "Compiling/Running tests...",
+  CompileError = "Compilation Error",
+  TestFailure = "Tests Failed",
+  Succeeded = "Compilation Succeeded/Tests Passed"
+}
+
+export const ReactMonacoEditor: React.FC<EditorProps> = ({
+    defaultCode = files['test.w'].file.contents,
+    hostname = 'localhost',
+    path = '/',
+    port = '3111',
+    className
+}) => {
+    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
+    const containerRef = useRef<WebContainer>();
+    const ref = createRef<HTMLDivElement>();
+    const [compileResult, setCompileResult] = useState("");
+    const [loadingStatus, setLoadingStatus] = useState(LoadingStatus.Init);
+
+    let lspWebSocket: WebSocket;
+
+    const editorWillMount = (monaco: any) => {
+    
+      try {
+        monaco.languages.register({ 
+          id: 'wing', 
+          extensions: ['.w', '.wing'],
+          aliases: ['Wing', 'wing']
+        });
+      } catch (error) {
+          console.error(error);
+      }
+  
+      monaco.editor.defineTheme('akkd-dark-plus', darkPlusTheme);
+    };
+
+    const editorDidMount = async (editor: any, monaco: any) => {
+      editorRef.current = editor
+      const lspWorker = new LspWorker()
+      
+      // install Monaco language client services
+      MonacoServices.install(monaco);
+
+      await wireGrammers(monaco);
+      monaco.editor.setModelLanguage(editor.getModel(), 'wing');
+      monaco.editor.setTheme('akkd-dark-plus');
+
+      // do not wait for webcontainers
+      initContainer().then(async instance => {
+        containerRef.current = instance
+        setLoadingStatus(LoadingStatus.Install)
+        await installDependencies(containerRef.current);
+        setLoadingStatus(LoadingStatus.Eval)
+        evaluateCode(undefined);
+      });
+
+      setTimeout(() => {
+        const reader = new BrowserMessageReader(lspWorker);
+        const writer = new BrowserMessageWriter(lspWorker);
+        const languageClient = createLanguageClient({ reader, writer });
+        languageClient.start();
+        
+        reader.onClose(() => languageClient.stop());
+        
+        function createLanguageClient(transports: any) {
+          return new MonacoLanguageClient({
+            name: 'Wing Language Client',
+            clientOptions: {
+              // use a language id as a document selector
+              documentSelector: [{ language: 'wing' }],
+              // disable the default error handler
+              errorHandler: {
+                error: () => ({ action: ErrorAction.Continue }),
+                closed: () => ({ action: CloseAction.DoNotRestart })
+              }
+            },
+            // create a language client connection to the server running in the web worker
+            connectionProvider: {
+              get: () => {
+                return Promise.resolve(transports);
+              }
+            }
+          });
+        }
+      }, 3000)
+  
+    };
+
+    const evaluateCode = debounce(async (value: string | undefined) => {
+      if (!containerRef.current) {
+        return
+      }
+      
+      console.log('evaluating...')
+
+      setCompileResult("")
+      setLoadingStatus(LoadingStatus.Eval)
+
+      await prepareForEvaluation(containerRef.current, value)
+
+      try {
+        const [compileResult, testCode] = await Promise.all([compile(containerRef.current), test(containerRef.current)])
+        if (testCode !== 0) {
+          setLoadingStatus(LoadingStatus.TestFailure)
+        } else {
+          setLoadingStatus(LoadingStatus.Succeeded)
+        }
+        setCompileResult(compileResult);
+      } catch (e) {
+        setLoadingStatus(LoadingStatus.CompileError)
+      }
+    }, 700)
+
+    const onChange = (value: string | undefined, ev: monaco.editor.IModelContentChangedEvent) => {
+      evaluateCode(value)
+    }
+
+    const getTestStyle = (status: LoadingStatus) => {
+      if (status === LoadingStatus.CompileError || status === LoadingStatus.TestFailure) {
+        return { background: "#f44747" }
+      } else if (status === LoadingStatus.Succeeded) {
+        return { background: "#4EC9B0" }
+      } else {
+        return { background: "#0000005c" }
+      }
+    }
+
+    useEffect(() => {
+        if (ref.current != null) {
+            return () => {
+                editorRef.current!.dispose();
+            };
+        }
+    }, []);
+
+
+    return (
+      <div>
+        <div className='editors'>
+          <Editor
+            height="60vh"
+            width="500px"
+            theme="vs-dark"
+            path={"source.w"}
+            defaultLanguage={"wing"}
+            defaultValue={defaultCode}
+            onMount={editorDidMount}
+            beforeMount={editorWillMount}
+            onChange={onChange}
+            />
+          <Editor
+            height="60vh"
+            width="500px"
+            theme="vs-dark"
+            defaultLanguage={"json"}
+            value={compileResult}
+            />
+        </div>
+        <div className="status" style={getTestStyle(loadingStatus)}>{loadingStatus}</div>
+      </div>
+    );
+};
---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    preserveSymlinks: true,
+    alias: {
+      "wasi-js/dist/bindings/node": "wasi-js/dist/bindings/browser",
+    },
+  },
+  plugins: [
+    react(), 
+    nodePolyfills(),
+    {
+      name: 'configure-response-headers',
+      configureServer: server => {
+        server.middlewares.use((_req, res, next) => {
+            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+            next();
+        });
+      },
+      configurePreviewServer: server => {
+        server.middlewares.use((_req, res, next) => {
+            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+            next();
+        });
+      }
+    }
+  ],
+  worker: {
+    format: "es",
+    plugins: [nodePolyfills()],
+    rollupOptions: {
+      preserveSymlinks: true,
+    },
+  },
+  build: {
+    target: "es2022",
+    commonjsOptions: {
+      // This is needed because winglang is symlinked
+      include: [/winglang/, /node_modules/],
+    },
+  },
+  server: {
+    fs: {
+      allow: [".."],
+    },
+  },
+  optimizeDeps: {
+    include: ["winglang"],
+    esbuildOptions: {
+      target: "es2022",
+      preserveSymlinks: true,
+    },
+    force: true,
+  }
+})
