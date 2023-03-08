4ac34a466b10c7991cc4b5e107a6471cd31590cd
Elad Cohen <ecohen@cloudbees.com>
2023-03-08 17:31:11 +0200

    use sdk base

--- src/containers.ts
+++ src/containers.ts
@@ -1,14 +1,26 @@
 import { WebContainer } from '@webcontainer/api';
 import files from './files';
+// import files2 from './files2';
+
+import pack1 from './assets/winglang-sdk-0.0.0.tgz?url'
+import pack2 from './assets/winglang-0.0.0.tgz?url'
+
+let pack1Data = await fetch(pack1).then((d) => d.arrayBuffer());
+let pack2Data = await fetch(pack2).then((d) => d.arrayBuffer());
 
 export async function initContainer(): Promise<WebContainer> {
   const webcontainerInstance = await WebContainer.boot();
-  await webcontainerInstance.mount(files);
+  await webcontainerInstance.mount(Object.assign({}, files as any, 
+    { 'winglang-sdk-0.0.0.tgz': { file: { contents: new Uint8Array(pack1Data) } } },
+    { 'winglang-0.0.0.tgz': { file: { contents: new Uint8Array(pack2Data) } } }
+  ));
   return webcontainerInstance
 }
 
 export async function installDependencies(webcontainerInstance: WebContainer) {
     // Install dependencies
+    // await webcontainerInstance.fs.writeFile('winglang-sdk-0.0.0.tgz', pack1Data)
+    // await webcontainerInstance.fs.writeFile('winglang-0.0.0.tgz', pack2Data)
     const installProcess = await webcontainerInstance.spawn('npm', ['install']);
     installProcess.output.pipeTo(
         new WritableStream({
@@ -18,10 +30,22 @@ export async function installDependencies(webcontainerInstance: WebContainer) {
         })
     );
     // Wait for install command to exit
-    return installProcess.exit;
+    await installProcess.exit;
+
+    const unzip = await webcontainerInstance.spawn('node', ['unzip.js']);
+    unzip.output.pipeTo(
+        new WritableStream({
+            write(data) {
+                console.log(data);
+            }
+        })
+    );
+    // Wait for unzip command to exit
+    return unzip.exit;
 }
 
 export async function compile(webcontainerInstance: WebContainer): Promise<string> {
+  return "asd"
   const compileProcess = await webcontainerInstance.spawn('npm', ['run', 'compile']);
   compileProcess.output.pipeTo(
       new WritableStream({
--- src/files.ts
+++ src/files.ts
@@ -1,4 +1,24 @@
 export default {
+  'unzip.js': {
+    file: {
+      contents: `
+console.log(1)
+
+
+import targz from 'tar.gz';
+
+// Using promises
+targz().extract('winglang-sdk-0.0.0.tgz', 'node_modules/@winglang')
+  .then(async function(){
+    await targz().extract('winglang-0.0.0.tgz', 'node_modules')
+    console.log('Job done!');
+  })
+  .catch(function(err){
+    console.log('Something is wrong ', err.stack);
+  });
+      `
+    }
+  },
   'test.w': {
     file: {
       contents: `bring cloud;
@@ -24,13 +44,20 @@ new cloud.Function(inflight (_: str) => {
           "name": "example-app",
           "type": "module",
           "dependencies": {
-            "winglang": "0.5.57",
-            "@winglang/sdk": "0.5.57",
-            "cdktf": "0.15.2"
+            "chalk": "^4.1.2",
+            "commander": "^10.0.0",
+            "compare-versions": "^5.0.3",
+            "constructs": "~10.1.228",
+            "debug": "^4.3.4",
+            "is-installed-globally": "^0.4.0",
+            "open": "^8.4.0",
+            "tar.gz": "^1.0.7",
+            "update-notifier": "^6.0.2",
+            "vscode-languageserver": "^8.0.2"
           },
           "scripts": {
-            "test": "wing test test.w",
-            "compile": "wing compile -t tf-aws test.w"
+            "test": "./node_modules/winglang/bin/wing test test.w",
+            "compile": "./node_modules/winglang/bin/wing compile -t tf-aws test.w"
           }
         }`,
     },
