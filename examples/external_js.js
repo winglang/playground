bd72c184518f9294e4efb9b79e3c99cabd946e7b
Eyal Keren <eyalk@monada.co>
2023-05-01 09:36:14 +0300

    Delete external_js.js

--- examples/external_js.js
+++
@@ -1,8 +0,0 @@
-exports.get_greeting = function(name) {
-  return `Hello, ${name}!`;
-};
-
-exports.regex_inflight = async function(pattern, text) {
-  const regex = new RegExp(pattern);
-  return regex.test(text);
-};
