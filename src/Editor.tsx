c9ada89ebcc3dab4cec75e3dfb7f73342a4ca1f5
Ainvoner <shaia@monada.co>
2023-05-09 18:48:53 +0300

    .

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -314,7 +314,7 @@ export const ReactMonacoEditor: React.FC<EditorProps> = ({
 
     const options: monaco.editor.IStandaloneEditorConstructionOptions = {
       minimap: { enabled: false },
-      fontSize: 18
+      fontSize: 16
     };
 
     const [steps] = useState(() => {
@@ -382,10 +382,13 @@ export const ReactMonacoEditor: React.FC<EditorProps> = ({
                       </div>
                   </div>
                   <div className='px-4 py-2 text-white border-t border-b border-r border-gray-400 flex gap-2'>
-                      <button className='px-2 py-0.5 hover:bg-gray-700 rounded' onClick={() => solveTutorial()}>Solve</button>
+                      {currentStep?.solution && <button className='px-2 py-0.5 hover:bg-gray-700 rounded'
+                               onClick={() => solveTutorial()}>Solve</button>}
                       <div className="grow"></div>
-                      <button className='px-2 py-0.5 hover:bg-gray-700 rounded' onClick={() => goToPreviousTutorial()}>Previous</button>
-                      <button className='px-2 py-0.5 hover:bg-gray-700 rounded' onClick={() => goToNextTutorial()}>Next</button>
+                      {currentStep?.id !== "01" && <button className='px-2 py-0.5 hover:bg-gray-700 rounded'
+                               onClick={() => goToPreviousTutorial()}>Previous</button>}
+                      {currentStep?.id !== "06" && <button className='px-2 py-0.5 hover:bg-gray-700 rounded'
+                               onClick={() => goToNextTutorial()}>Next</button>}
                   </div>
               <div className='grow w-full relative'>
                 <div className="absolute inset-0 overflow-hidden">
