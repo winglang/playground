a060ca073034f73828f49d0118dbe6aa7db3480b
Shai Ber <shaib@monada.co>
2023-05-14 22:16:50 +0300

    revert

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -486,13 +486,10 @@ export const ReactMonacoEditor: React.FC<EditorProps> = ({
                 <h1 className='text-[1.7rem]'>Welcome to the Winglang Tutorial!</h1>
                 <div className='pt-8 space-y-8'>
                   <div>
-                  Let's build a smart queue that prints its messages and stores the latest one in a bucket.
+                  Let's quickly build a smart queue that prints its messages and stores the latest one in a bucket.
                   </div>
                   <div>
                   You'll interact with it in the embedded Wing Simulator, and then compile to AWS.
-                  </div>
-                  <div>
-                  ⏱️ 4-8 min.
                   </div>
                   <div>
                     <button className='mt-2 px-4 py-4 w-full hover:bg-[#2AD5C1] hover:text-slate-800 bg-gray-600 rounded-lg' onClick={() => setShowWelcomeModal(false)}>
