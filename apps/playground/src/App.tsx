956495a6bba221b19a909deaeca68cb614db7635
Cristian Pallarés <cristianp@monada.co>
2023-05-18 09:47:57 +0200

    wip

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,17 +1,16 @@
-import './App.css'
 import { ReactMonacoEditor } from './Editor'
 import { Header } from './Header'
-import { isChrome } from './utils'
+import { isChrome } from '@wing-playground/shared/src/utils'
 import { VscWarning } from "react-icons/vsc";
 
 function App() {
   return (
     <div className="max-h-full h-full flex flex-col text-slate-700">
       <Header />
-      { isChrome() ? <ReactMonacoEditor /> : 
+      { isChrome() ? <ReactMonacoEditor /> :
       <div className='h-full flex justify-center content-center items-center'>
         <div className='h-24 text-xl flex flex-row justify-center content-center items-center text-[#f1f0f1]'>
-          <VscWarning className='text-amber-500'/> 
+          <VscWarning className='text-amber-500'/>
           <span>This playground uses <a href="https://webcontainers.io/" className='text-teal-500'> Web Containers </a> and only works on Google Chrome.</span>
         </div>
       </div> }
