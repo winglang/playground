88d5a3d5ef8e8982e7460f49ca2fb9b56736b960
Cristian Pallarés <cristianp@monada.co>
2023-05-22 09:31:19 +0200

    wip

--- packages/shared/src/containers.ts
+++ packages/shared/src/containers.ts
@@ -3,8 +3,8 @@ import files from './files';
 
 import expressUrl from './assets/express.tgz?url'
 import tarGzCode from "./assets/tar.gz.js?url";
-import winglangSdkUrl from '@wing-playground/console-build/wing/winglang-sdk-webpack.tgz?url'
-import winglangUrl from '@wing-playground/console-build/wing/winglang-webpack.tgz?url'
+import winglangSdkUrl from '@wing-playground/console-build/dist/winglang-sdk-webpack.tgz?url'
+import winglangUrl from '@wing-playground/console-build/dist/winglang-webpack.tgz?url'
 import codespanWasmUrl from '@wing-playground/console-build/wing/codespan-wasm.tgz?url'
 import vm2Url from '@wing-playground/console-build/wing/vm2.tgz?url'
 import consoleCode from "@wing-playground/console-build/dist/console.server.js?url";
