ea6a07944ae179f892d46d509b94e84c8d4ccd7d
Cristian Pallarés <cristianp@monada.co>
2023-05-22 09:59:53 +0200

    chore: fix turbo cache (#104)
    Moves some files required for the build process to `dist/` so they're
    cached by turbo's cache.

--- packages/shared/src/lsp/lsp.ts
+++ packages/shared/src/lsp/lsp.ts
@@ -1,6 +1,6 @@
 import { load, invoke } from "winglang";
 import { createFsFromVolume } from "@cowasm/memfs";
-import wingcURL from "@wing-playground/console-build/wing/wingc.wasm?url";
+import wingcURL from "@wing-playground/console-build/dist/wingc.wasm?url";
 import { Volume } from "@cowasm/memfs";
 import { Buffer } from "buffer";
 
