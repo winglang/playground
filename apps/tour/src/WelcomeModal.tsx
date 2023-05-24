e46538316253d44400ca6bf6a287fb0b8687bfa8
Eyal Keren <eyalk@monada.co>
2023-05-24 16:19:35 +0300

    tour v2

--- apps/tour/src/WelcomeModal.tsx
+++ apps/tour/src/WelcomeModal.tsx
@@ -8,14 +8,14 @@ export const WelcomeModal = ({visible, onClose}: {visible: boolean, onClose: () 
                 <h1 className='text-[1.7rem]'>Welcome to the Winglang Tutorial!</h1>
                 <div className='pt-8 space-y-8'>
                     <div>
-                        Let's quickly build a smart queue that prints its messages and stores the latest one in a bucket.
+                        Let's quickly learn about Winglang
                     </div>
                     <div>
-                        You'll interact with it in the embedded Wing Simulator, and then compile to AWS.
+                        You'll create resources, explore the terraform output, interact with the Simulator and have fune 
                     </div>
                     <div>
                         <button className='mt-2 px-4 py-4 w-full hover:bg-[#2AD5C1] hover:text-slate-800 bg-gray-600 rounded-lg' onClick={onClose}>
-                            Let’s get started!
+                            Start Learning Winglang
                         </button>
                     </div>
                 </div>
