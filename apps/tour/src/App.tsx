31d069bc9226c741c0609bf5bde5082f614cb0ee
Eyal Keren <eyalk@monada.co>
2023-05-30 18:28:24 +0300

    setting default tour to topic

--- apps/tour/src/App.tsx
+++ apps/tour/src/App.tsx
@@ -12,7 +12,7 @@ import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom
 const tutorialRoutes = [
   {
     path: '/',
-    tutorials: queueTutorial
+    tutorials: topicTutorial
   }, {
     path: '/bucket',
     tutorials: bucketTutorial
