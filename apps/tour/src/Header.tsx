a1e1fe9647182dfb60006b9d6191358fc6144d43
polamoros <info@polamoros.com>
2023-06-08 12:59:20 +0200

    wip

--- apps/tour/src/Header.tsx
+++ apps/tour/src/Header.tsx
@@ -5,10 +5,9 @@ export const Header = () => {
     <div className={
       classNames("py-4 flex gap-12 items-center font-sans text-[#BDCECC] bg-[#293443]")
     }>
-      <div className="flex items-center gap-x-2">
-        <img src="/turquoise.svg" className='w-10' />
-        <span className="text-xl">Wing</span>
-      </div>
+      <a href="https://winglang.io/" target="_blank" rel="noreferrer">
+          <img src="/wing_logo.svg" className='h-6' />
+      </a>
       <a href="https://docs.winglang.io/" target="_blank" rel="noreferrer">
         Docs
       </a>
