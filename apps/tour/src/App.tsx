fdce3ea5d455b5843f4fd7d9e1a745f16008e1d6
polamoros <info@polamoros.com>
2023-06-09 14:23:09 +0200

    remove chrome restriction

--- apps/tour/src/App.tsx
+++ apps/tour/src/App.tsx
@@ -31,14 +31,15 @@ const tutorialRoutes = [
 function AppView({ tutorials }: { tutorials: Tutorial[] }) {
   return (
     <div className="max-h-full h-full flex flex-col text-slate-700">
-      { isChrome() ? <ReactMonacoEditor tutorials={tutorials} /> :
+      <ReactMonacoEditor tutorials={tutorials} />
+      {/* { isChrome() ? <ReactMonacoEditor tutorials={tutorials} /> :
       <div className='h-full flex justify-center content-center items-center'>
         <div className='h-24 text-xl flex flex-row justify-center content-center items-center text-[#f1f0f1]'>
           <VscWarning className='text-amber-500'/>
           <span>This playground uses <a href="https://webcontainers.io/" className='text-teal-500'> Web Containers </a> and only works on Google Chrome.</span>
           </div>
-      </div> }
-        </div>
+      </div> } */}
+    </div>
   )
 }
 
