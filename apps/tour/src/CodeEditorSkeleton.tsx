0211994b0135f3e50d57436f507a99cb3b38ba0c
polamoros <info@polamoros.com>
2023-06-09 12:55:35 +0200

    wip

--- apps/tour/src/CodeEditorSkeleton.tsx
+++ apps/tour/src/CodeEditorSkeleton.tsx
@@ -1,21 +1,21 @@
 export const CodeEditorSkeleton = () => {
   return (
     <div className='flex flex-col h-full w-full relative'>
-      <div className="absolute inset-0 overflow-hidden flex flex-col p-[33px] gap-[5px]">
-        <div className="flex gap-x-[25px] h-6">
-          <div className="w-6 bg-gray-700 rounded-sm shrink-0"/>
+      <div className="absolute inset-0 overflow-hidden flex flex-col p-[33px] gap-[8px]">
+        <div className="flex gap-x-[25px] h-5">
+          <div className="w-5 bg-gray-700 rounded-sm shrink-0"/>
           <div className="w-[110px] bg-gray-700 rounded-sm shrink-0"/>
         </div>
-        <div className="flex gap-x-[25px] h-6">
-          <div className="w-6 bg-gray-700 rounded-sm shrink-0"/>
+        <div className="flex gap-x-[25px] h-5">
+          <div className="w-5 bg-gray-700 rounded-sm shrink-0"/>
           <div className="w-[0px] bg-gray-700 rounded-sm shrink-0"/>
         </div>
-        <div className="flex gap-x-[25px] h-6">
-          <div className="w-6 bg-gray-700 rounded-sm shrink-0"/>
+        <div className="flex gap-x-[25px] h-5">
+          <div className="w-5 bg-gray-700 rounded-sm shrink-0"/>
           <div className="w-[300px] bg-gray-700 rounded-sm shrink-0"/>
         </div>
-        <div className="flex gap-x-[25px] h-6">
-          <div className="w-6 bg-gray-700 rounded-sm shrink-0"/>
+        <div className="flex gap-x-[25px] h-5">
+          <div className="w-5 bg-gray-700 rounded-sm shrink-0"/>
           <div className="w-[0px] bg-gray-700 rounded-sm shrink-0"/>
         </div>
       </div>
