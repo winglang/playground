e734c9921227d788ad9834c3f21ddb16e4e5a268
polamoros <info@polamoros.com>
2023-06-09 10:51:14 +0200

    wip

--- apps/tour/src/Editor.tsx
+++ apps/tour/src/Editor.tsx
@@ -333,11 +333,12 @@ export const ReactMonacoEditor: React.FC<EditorProps> = ({tutorials = mainTutori
                               <div
                                 className={classNames(
                                   "absoulte z-20 top-0 left-0 bg-[#2AD5C1] h-[4px] -translate-y-1/2",
-                                  "transition-all duration-300 ease-out"
+                                  "transition-all duration-300 ease-out",
+                                  showWelcome && "opacity-0"
                                 )}
                                 style={
                                   {
-                                    width: `${currentStepIndex / tutorials.length * 100}%`
+                                    width: `${(currentStepIndex + 1) / tutorials.length * 100}%`
                                   }
                                 }
                               />
