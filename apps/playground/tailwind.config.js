8fdbe5b2b8fd6d6a8d336f7f24ad9b66f05c5146
polamoros <info@polamoros.com>
2023-06-12 17:27:38 +0200

    wip

--- apps/playground/tailwind.config.js
+++ apps/playground/tailwind.config.js
@@ -65,7 +65,7 @@ module.exports = {
   plugins: [require("@tailwindcss/typography")],
   theme: {
     fontFamily: {
-      sans: "IBM Plex Sans",
+      sans: ["IBM Plex Sans"],
     },
     extend: {
       colors: {
