1921715ac281cce7b2da199f52c4ad91dfdc5fc8
polamoros <info@polamoros.com>
2023-06-14 11:28:03 +0200

    wip

--- packages/shared/src/ThemeToggle.tsx
+++ packages/shared/src/ThemeToggle.tsx
@@ -1,18 +1,28 @@
-import { useMemo } from 'react';
-
 import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 import { setCurrentTheme, useTheme } from './theme-provider';
+import classNames from 'classnames';
+import {useCallback} from 'react';
 
 export const ThemeToggle = () => {
-
   const { theme, mode } = useTheme();
+
+  const toggleTheme = useCallback(() => {
+    setCurrentTheme(mode === 'dark' ? 'light' : 'dark');
+  }, [mode]);
+
   return (
-    <button className='rounded text-white bg-slate-400 dark:bg-slate-700 p-1' onClick={() => {
-      const nextTheme = mode === "light" ? "dark" : "light";
-      setCurrentTheme(nextTheme);
-    }}>
-      { mode === "light" && <SunIcon className='w-5 h-5'/> }
-      { mode === "dark" && <MoonIcon className='w-5 h-5'/> }
+    <button
+      className={
+        classNames(
+          "p-1.5 rounded-3xl font-medium",
+          "text-slate-900 dark:text-white",
+          "hover:bg-slate-200 hover:dark:bg-slate-700"
+      )}
+      onClick={() => toggleTheme()}
+
+    >
+      {mode === 'light' && <SunIcon className='w-6 h-6'/>}
+      {mode === 'dark' && <MoonIcon className='w-6 h-6'/>}
     </button>
   );
 }
