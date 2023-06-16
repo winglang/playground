d0fee27004a39914b1122d90e735dcc5b4263522
Cristian Pallarés <cristianp@monada.co>
2023-06-16 14:11:18 +0200

    fix: analytics not being reported

--- packages/shared/container-files/index.js
+++ packages/shared/container-files/index.js
@@ -1,9 +1,8 @@
 const { createConsoleApp } = require("@wingconsole/app");
 
-(async () => {
-  const server = await createConsoleApp({
-    wingfile: "test.w",
-  });
-
-  console.log(`Server is running on http://localhost:${server.port}.`);
-})();
+createConsoleApp({
+  wingfile: "test.w",
+  onTrace(trace) {
+    window.parent.postMessage({ trace }, "*");
+  },
+});
