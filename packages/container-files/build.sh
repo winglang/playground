98298797b3988c3ab5e6e571896600bee6e8a741
Elad Cohen <ecohen@cloudbees.com>
2023-06-21 16:59:37 +0300

    run build as shell script

--- packages/container-files/build.sh
+++ packages/container-files/build.sh
@@ -3,5 +3,9 @@
 pushd lib
 pnpm --version
 pnpm install
+rm -rf .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@aws-sdk .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@azure .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/cdktf
 popd
-tsx scripts/updatePlayground.ts
+
+rm -rf dist
+mkdir -p dist
+tar zcf dist/playground.tgz -C ./lib .
