d8c5286b7f2952b5fb995b0ed8f95b47f2bafc35
Elad Cohen <ecohen@cloudbees.com>
2023-06-21 17:59:53 +0300

    abs dir path

--- packages/container-files/build.sh
+++ packages/container-files/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-pushd lib
+dir="$( cd "$( dirname "$0" )" && pwd )"
+
+pushd "${dir}/lib"
 pnpm --version
 pnpm install
 rm -rf .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@aws-sdk .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@azure .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/cdktf
 
 popd
-rm -rf dist
-mkdir -p dist
-tar zcf dist/playground.tgz -C ./lib .
+rm -rf "${dir}/dist"
+mkdir -p "${dir}/dist"
+tar zcf "${dir}/dist"/playground.tgz -C "${dir}/lib" .
