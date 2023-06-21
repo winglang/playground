f1c62699c68e95a582ae716793385149a81d3173
Elad Cohen <ecohen@cloudbees.com>
2023-06-21 17:14:22 +0300

    remove scripts

--- packages/container-files/build.sh
+++ packages/container-files/build.sh
@@ -4,8 +4,8 @@ pushd lib
 pnpm --version
 pnpm install
 rm -rf .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@aws-sdk .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@azure .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/cdktf
-popd
 
+popd
 rm -rf dist
 mkdir -p dist
 tar zcf dist/playground.tgz -C ./lib .
