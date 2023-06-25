4e5ec248a65eb491372401cc34435e973746129c
eladcon <eladcon@gmail.com>
2023-06-25 18:10:28 +0300

    delete node_modules

--- packages/container-files/build.sh
+++ packages/container-files/build.sh
@@ -4,6 +4,7 @@ dir="$( cd "$( dirname "$0" )" && pwd )"
 
 pushd "${dir}/lib"
 pnpm --version
+rm -rf node_modules pnpm-lock.yaml
 pnpm install
 rm -rf .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@aws-sdk .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/@azure .//node_modules/.pnpm/*/node_modules/@winglang/sdk/node_modules/cdktf
 
