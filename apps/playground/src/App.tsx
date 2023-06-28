836e4b670f2d1061b344506ba93a9ff5eafa0a05
polamoros <info@polamoros.com>
2023-06-28 15:01:59 +0200

    feat: add message if device is not supported in playground/tutorial (#193)
    resolves https://github.com/winglang/wing/issues/2499
    
    ---------
    
    Co-authored-by: Eyal Keren <eyalk@monada.co>

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,23 +1,38 @@
-import { useEffect } from 'react';
+import { useAnalytics } from '@wing-playground/shared/src/analytics/use-analytics';
 import { ReactMonacoEditor } from './Editor'
 import { usePreventSave } from "@wing-playground/shared/src/use-prevent-save";
+import { getBrowser, isChrome } from '@wing-playground/shared/src/utils';
+import { LoadingStatus } from '@wing-playground/shared/src/loading-status';
 
 
 function App() {
   usePreventSave({enable: true});
 
+  const chrome = isChrome();
+  if (!chrome) {
+    const { analytics } = useAnalytics({ name: "playground", state: LoadingStatus.Completed });
+    analytics.track("playground: blocked for device", {
+      device: getBrowser()
+    });
+  }
+
   return (
     <div className="max-h-full h-full flex flex-col">
-      <ReactMonacoEditor />
-      {/* { isChrome() ? <ReactMonacoEditor /> :
-      <div className='h-full flex justify-center content-center items-center'>
-        <div className='h-24 text-xl flex flex-row justify-center content-center items-center text-[#f1f0f1]'>
-          <VscWarning className='text-amber-500'/>
-          <span>This playground uses <a href="https://webcontainers.io/" className='text-teal-500'> Web Containers </a> and only works on Google Chrome.</span>
+      {chrome && <ReactMonacoEditor/>}
+      {!chrome &&
+        <div className='h-full flex justify-center content-center items-center bg-[#293443]'>
+          <div className='h-24 text-xl flex-row justify-center content-center items-center text-[#f1f0f1] text-center leading-relaxed'>
+            <div>
+              <span>The Winglang Playground experience is compatible exclusively with Chrome on desktop computers.</span>
+            </div>
+            <div>
+              <span>Please open this page in Chrome, or consider trying our  <a href="https://www.winglang.io/docs" className='text-teal-500'> Getting Started guide.</a></span>
+            </div>
+            </div>
         </div>
-      </div> } */}
+      }
     </div>
   )
 }
 
-export default App
+export default App;
