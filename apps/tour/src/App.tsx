53ce5f998de5df0fce27790ab4076016841701e3
polamoros <info@polamoros.com>
2023-06-28 09:33:18 +0200

    Update apps/tour/src/App.tsx
    Co-authored-by: Eyal Keren <eyalk@monada.co>

--- apps/tour/src/App.tsx
+++ apps/tour/src/App.tsx
@@ -35,7 +35,7 @@ function AppView({ tutorial }: { tutorial: Tutorial }) {
         <div className='h-full flex justify-center content-center items-center bg-[#293443]'>
           <div className='h-24 text-xl flex-row justify-center content-center items-center text-[#f1f0f1] text-center leading-relaxed'>
             <div>
-              <span>Winglang learn experience works only on chrome on desktops.</span>
+              <span>The Winglang Learn experience is compatible exclusively with Chrome on desktop computers.</span>
             </div>
             <div>
             <span>Please open this page in Chrome, or consider trying our  <a href="https://www.winglang.io/docs" className='text-teal-500'> Getting Started guide.</a></span>
