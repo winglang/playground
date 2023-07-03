38a91d841daceaa1a5016df6e9049c272aa03634
Shai Ber <shaib@monada.co>
2023-07-03 14:21:27 +0300

    better file picker color fix

--- packages/shared/src/FilePicker.tsx
+++ packages/shared/src/FilePicker.tsx
@@ -23,7 +23,7 @@ export function FilePicker({ examples, currentExample, setCurrentExample, setLan
   return (
     <div className="group">
       <div className="dropdown inline-block relative">
-        <button className="dark:text-[#FFFFFF] light:text-[#000000] py-2 px-4 rounded inline-flex items-center">
+        <button className="py-2 px-4 rounded inline-flex items-center">
           <span className="mr-1">{(!touched && currentExample.value !== examples[0].value) ? 'Examples' : currentExample.text}</span>
           <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
         </button>
