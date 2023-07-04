8fd3732d9d99490fb025ac7b19f9736ace030956
polamoros <info@polamoros.com>
2023-07-04 11:34:47 +0200

    fix scrolling

--- packages/shared/src/SimulatorTarget.tsx
+++ packages/shared/src/SimulatorTarget.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import {useEffect, MutableRefObject} from "react";
 
 export interface SimulatorTargetProps {
-    frameSrc: string;
-    iframeRef: React.MutableRefObject<any>
+  frameSrc: string;
+  iframeRef: MutableRefObject<any>
 }
 
 export const SimulatorTarget = (props: SimulatorTargetProps) => {
-    return (
-        <div className={"h-full"}>
-            <iframe
-                id='console'
-                className='w-full h-full basis-auto'
-                src={props.frameSrc}
-                allowFullScreen={true}
-                ref={props.iframeRef}
-            ></iframe>
-        </div>
-    )
+  // use useEffect to set iframe html to no overflow
+
+  useEffect(() => {
+    console.log('SimulatorTarget useEffect');
+    const iframe = props.iframeRef.current;
+    if (!iframe) {
+      return;
+    }
+    const iframeDocument = iframe.contentDocument;
+    if (!iframeDocument) {
+      return;
+    }
+    const iframeHtml = iframeDocument.documentElement;
+    if (!iframeHtml) {
+      return;
+    }
+    iframeHtml.style.overflow = 'hidden';
+  }, [props.iframeRef]);
+
+
+  return (
+    <div className="h-full">
+      <iframe
+        id='console'
+        className='basis-auto w-full h-full'
+        src={props.frameSrc}
+        allowFullScreen={true}
+        ref={props.iframeRef}
+        scrolling="no"
+      ></iframe>
+    </div>
+  )
 }
