33a89094b9210557e54e98b9fc1909e2f55dd746
Ainvoner <2538825+ainvoner@users.noreply.github.com>
2023-07-04 17:04:57 +0300

    chore: get out dir as env var (#210)
    default to dist

--- apps/tour/vite.config.ts
+++ apps/tour/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 const base_path = process.env.TOUR_BASE_PATH || "./";
+const out_dir = process.env.TOUR_OUT_DIR;
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -43,7 +45,7 @@ export default defineConfig({
   },
   build: {
     target: "es2022",
-
+    outDir: out_dir,
     commonjsOptions: {
       // This is needed because winglang is symlinked
       include: [
