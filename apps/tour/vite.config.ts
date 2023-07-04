3a83db401c4a1d79ba93aaabf9888b3b34877b93
Ainvoner <shaia@monada.co>
2023-07-04 10:48:59 +0200

    chore: support base path configuration value from env var

--- apps/tour/vite.config.ts
+++ apps/tour/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
+const base_path = process.env.BASE_PATH || "./";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: base_path,
   resolve: {
     // preserveSymlinks: true,
     alias: {
