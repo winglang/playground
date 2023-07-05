2aefd792b1780704562fdbfcfc1868148f7aa188
Ainvoner <shaia@monada.co>
2023-07-05 09:45:26 +0200

    Update vite.config.ts

--- apps/tour/vite.config.ts
+++ apps/tour/vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-const base_path = process.env.VITE_TOUR_BASE_PATH || "./";
 const out_dir = process.env.VITE_TOUR_OUT_DIR;
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: base_path,
+  base: "/learn",
   resolve: {
     // preserveSymlinks: true,
     alias: {
@@ -46,7 +43,7 @@ export default defineConfig({
   },
   build: {
     target: "es2022",
-    outDir: out_dir,
+    outDir: "dist/learn",
     commonjsOptions: {
       // This is needed because winglang is symlinked
       include: [
