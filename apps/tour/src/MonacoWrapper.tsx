1f24133deba3273627f02db9a8bbd858eb7b9086
Shai Ber <shaib@monada.co>
2023-07-05 17:29:01 +0300

    cleanup monaco wrapper

--- apps/tour/src/MonacoWrapper.tsx
+++ apps/tour/src/MonacoWrapper.tsx
@@ -1,25 +1,4 @@
-import React, { useEffect, useRef } from 'react';
 import Editor from '@monaco-editor/react';
-import convertTheme from "../../../packages/shared/src/monaco-themes/convert-tmtheme";
-import darkPlusTMTheme from "../../../packages/shared/src/monaco-themes/dark_plus";
-import lightPlusTMTheme from "../../../packages/shared/src/monaco-themes/light_plus";
-import wingLanguageConfiguration from '../../../packages/shared/src/language-configurations/wing-configration.json';
-import 'monaco-editor/esm/vs/editor/editor.all.js';
-
-// support all editor features
-import 'monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast.js';
-
-import * as monaco from 'monaco-editor';
-
 
 function countLines(str) {
     return (str.match(/\n/g) || '').length + 1;
@@ -35,72 +14,7 @@ const wrapperStyle = {
     marginBottom: '-1em',
   };
 
-const MonacoWrapper = ({ language, value, editorOptions, currentMode }) => {
-    const compileEditorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
-
-    const darkPlusTheme = convertTheme(darkPlusTMTheme);
-    const lightPlusTheme = convertTheme(lightPlusTMTheme);
-
-    let willMounts = 0;
-    let mounts = 0;
-
-    const editorWillMount = (monaco: any) => {
-        if (willMounts > 0) {
-            return;
-        }
-        try {
-            monaco.languages.register({
-                id: 'wing',
-                extensions: ['.w', '.wing'],
-                aliases: ['Wing', 'wing']
-            });
-
-            monaco.languages.setLanguageConfiguration('wing', wingLanguageConfiguration)
-        } catch (error) {
-            console.error(error);
-        }
-        //console.log('editor theme', editorTheme);
-        monaco.editor.defineTheme('dark', darkPlusTheme);
-        monaco.editor.defineTheme('light', lightPlusTheme);
-        willMounts++;
-    };
-
-    const editorDidMount = (editor, monaco) => {
-        if (mounts > 0) {
-            return;
-        }
-        compileEditorRef.current = editor
-
-        monaco.editor.defineTheme('myCustomTheme', {
-            base: currentMode === 'dark' ? 'vs-dark' : 'vs', // can also be vs or hc-black
-            inherit: true, // can also be false to completely replace the builtin rules
-            rules: [],
-            colors: currentMode === 'dark' ? 
-            {
-                'editor.background': '#334155', // Match your editor's background color here
-                'editor.lineHighlightBackground': '#334155' // Match your editor's background color here
-            } :
-            {
-                'editor.background': '#FFFFFF', // Match your editor's background color here
-                'editor.lineHighlightBackground': '#FFFFFF' // Match your editor's background color here
-            }
-          });
-        
-        editor.updateOptions({ theme: 'myCustomTheme' });
-
-        // Get the editor's DOM node
-        const editorDomNode = editor.getDomNode();
-
-        // Add a wheel event listener that prevents the default behavior
-        if (editorDomNode) {
-            editorDomNode.addEventListener('wheel', function(e) {
-            e.preventDefault();
-            }, { passive: false }); // use passive: false to make preventDefault() work in passive mode
-        }
-        
-        mounts++;
-    };
-    
+const MonacoWrapper = ({ language, value, editorOptions, currentMode }) => {    
     const lines = countLines(value);
     const height = `${1.7*lines + 1}vh`;
 
@@ -127,10 +41,8 @@ const MonacoWrapper = ({ language, value, editorOptions, currentMode }) => {
                         renderLineHighlight: 'none',
                     }}
                     height={height}
-                    defaultLanguage={language}
+                    language={language}
                     value={value}
-                    onMount={editorDidMount}
-                    beforeMount={editorWillMount}
                 />
             </div>
         </div>
