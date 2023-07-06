b410cc624c5ce9cb604cd6b95748abacded39b09
Shai Ber <shaib@monada.co>
2023-07-06 19:04:12 +0300

    fix build

--- apps/tour/src/App.tsx
+++ apps/tour/src/App.tsx
@@ -4,7 +4,7 @@ import { tutorial as bucketTutorial } from './tutorials/resource/bucket';
 import { tutorial as counterTutorial } from './tutorials/resource/counter';
 import { tutorial as topicTutorial } from './tutorials/resource/topic';
 import { tutorial as queueTutorial } from './tutorials/resource/queue';
-import { tutorial as inflightTutorial } from './tutorials/inflight';
+import { tutorial as inflightTutorial } from './tutorials/inflight/basic';
 import { tutorial as markdownPlayground } from './tutorials/markdown-playground';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import { usePreventSave } from "@wing-playground/shared/src/use-prevent-save";
