29ef360c8bc5d826d949805ebec96ded2d4089ce
Ainvoner <shaia@monada.co>
2023-07-06 12:17:18 +0200

    fix: play relative paths to shared assets

--- packages/shared/src/Loading.tsx
+++ packages/shared/src/Loading.tsx
@@ -1,6 +1,8 @@
 import { FiCommand } from "react-icons/fi";
 import classNames from "classnames";
 
+const routePrefix = import.meta.env.VITE_TOUR_BASE_PATH === '/learn' ? '/learn/' : '';
+
 export interface LoadingProps {
   status: string;
 }
@@ -12,7 +14,7 @@ export function Loading({ status }: LoadingProps) {
     "transition-colors duration-300",
   )}>
     <div className="mt-[20%]">
-      <img className="h-[64px] w-[136px]" src="/learn/wing_logo.svg" loading="lazy" alt="" />
+      <img className="h-[64px] w-[136px]" src={`${routePrefix}wing_logo.svg`} loading="lazy" alt="" />
     </div>
     <div className="flex items-center">
       <FiCommand className="animate-spin"/>
