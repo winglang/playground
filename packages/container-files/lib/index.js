97cd31e5fcba9a525900c89df6d40a0dcfaf2a72
eladcon <eladcon@gmail.com>
2023-07-10 15:27:48 +0300

    chore: create custom console app (#221)
    Use @wingconsole/server and @wingconsole/ui to create a custom app with
    fullstory
    
    Fixes winglang/wing#3042

--- packages/container-files/lib/index.js
+++ packages/container-files/lib/index.js
@@ -1,9 +1,17 @@
-const { createConsoleApp } = require("@wingconsole/app");
+const { createConsoleServer } = require("@wingconsole/server");
+const express = require("express");
+
+const staticDir = `${__dirname}/dist`;
 
 (async () => {
-  const server = await createConsoleApp({
+
+  const server = await createConsoleServer({
     wingfile: "test.w",
-  });
+    log: console,
+    onExpressCreated: (app) => {
+      app.use(express.static(staticDir))
+    }
+  })
 
   console.log(`Server is running on http://localhost:${server.port}.`);
 })();
