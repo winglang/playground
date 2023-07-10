84c8a56cab3f9963355520e295c8f37655e42dcd
Ainvoner <2538825+ainvoner@users.noreply.github.com>
2023-07-10 14:28:16 +0300

    feat: add play as path to winglang.io domain (#230)
    update paths and rewrites for supporting play.winglang.io as a path in
    our main winglang.io domain
    - [x] depends on a website-proxy PR
    
    ---------
    
    Co-authored-by: Cristian Pallarés <cristianp@monada.co>

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
+const base_path = process.env.VITE_BASE_PATH || "./";
+const out_dir = process.env.VITE_OUT_DIR;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base_path,
   resolve: {
     // preserveSymlinks: true,
     alias: {
@@ -39,6 +43,7 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: out_dir,
     target: "es2022",
 
     commonjsOptions: {
