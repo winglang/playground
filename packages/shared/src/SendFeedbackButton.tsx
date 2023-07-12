15fad8715ff1ac541c1988b745080caf358233e8
polamoros <info@polamoros.com>
2023-07-12 17:20:02 +0200

    wip

--- packages/shared/src/SendFeedbackButton.tsx
+++ packages/shared/src/SendFeedbackButton.tsx
@@ -2,7 +2,7 @@ import { useTheme } from './theme-provider';
 import classNames from 'classnames';
 
 import { useCallback } from 'react';
-import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleLeftRightIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 
 export interface SendFeedbackButtonProps {
   onClick: (url: URL) => void;
@@ -20,15 +20,19 @@ export const SendFeedbackButton = ({ onClick, className } : SendFeedbackButtonPr
   return (
     <button
       className={classNames(
-        "flex items-center justify-center h-[26px] pb-[2px] cursor-pointer",
+        "flex items-center justify-center cursor-pointer",
         theme.textInput,
+        "p-1.5 rounded-3xl font-medium flex focus:outline-none",
+        "hover:bg-slate-200 hover:dark:bg-slate-700",
+        "transition-color duration-300",
         className,
       )}
       onClick={handleClick}
       title="Send Feedback"
     >
-      <div className="h-[24px] w-[24px] leading-[20px]">
+      <div className="relative h-[24px] w-[24px] leading-[20px]">
         <ChatBubbleLeftRightIcon className="w-[24px] h-[24px]" />
+        <PlusSmallIcon className="w-[7px] absolute top-[9.5px] left-[12.5px]" />
       </div>
     </button>
   );
