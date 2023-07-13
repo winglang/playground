905b99cce3008475f780be9b36e1aa7a14773116
Elad Cohen <ecohen@cloudbees.com>
2023-07-13 11:34:27 +0300

    error message

--- apps/tour/src/App.tsx
+++ apps/tour/src/App.tsx
@@ -57,10 +57,10 @@ function AppView({ tutorial }: { tutorial: Tutorial }) {
         <div className='h-full flex justify-center content-center items-center bg-[#293443]'>
           <div className='h-24 text-xl flex-row justify-center content-center items-center text-[#f1f0f1] text-center leading-relaxed'>
             <div>
-              <span>Unfortunately, the Wing Tutorial is only compatible with Chrome/Safari for desktop at the moment.</span>
+              <span>Unfortunately, the Wing Tutorial is only compatible with desktop Chromium-based, Firefox and Safari &gt; 16.4 at the moment.</span>
             </div>
             <div>
-              <span>Please open this page in Chrome, or <a href="https://www.winglang.io/docs" className='text-teal-500'>get started</a> with Wing on your system.</span>
+              <span>Please open this page in a different browser, or <a href="https://www.winglang.io/docs" className='text-teal-500'>get started</a> with Wing on your system.</span>
             </div>
             </div>
         </div>
