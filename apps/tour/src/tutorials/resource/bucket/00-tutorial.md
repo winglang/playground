700308305dc932932ba3c31c34534600ec4502ac
Eyal Keren <eyalk@monada.co>
2023-07-16 14:36:22 +0300

    update

---
+++ apps/tour/src/tutorials/resource/bucket/00-tutorial.md
@@ -0,0 +1,11 @@
+## Welcome to the Bucket tutorial
+
+This tutorial is a straightforward walkthrough explaining `cloud.Bucket` and
+its use cases.
+
+We'll cover how to create a `cloud.Bucket`, examine the Terraform assets that
+represent it, and use the inflight/preflight API to understand its
+capabilities. Lastly, we'll create a simple application that stores copies of
+the files uploaded to one bucket in another bucket.
+
+This should be a fun learning experience. Click **START** to begin.
--- apps/tour/src/tutorials/resource/bucket/index.ts
+++ apps/tour/src/tutorials/resource/bucket/index.ts
@@ -1,4 +1,7 @@
 import { Target } from "@wing-playground/shared/src/compiler/compiler";
+
+import tutorial00 from "./00-tutorial.md?raw";
+
 import code01 from "./01-code.w?raw";
 import tutorial01 from "./01-tutorial.md?raw";
 
@@ -16,6 +19,12 @@ import tutorial05 from "../../main/07-tutorial.md?raw";
 import {Tutorial, TutorialPage} from "../../main";
 
 export const tutorialPages: TutorialPage[] = [
+      {
+        id: "0",
+        name:   "Welcome",
+        code: code01,
+        tutorial: tutorial00,
+    },
     {
         id: "1",
         name:   "Bucket Definition",
