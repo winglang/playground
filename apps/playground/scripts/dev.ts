92919fcab13b359c1872fc7398c16032fc9d6813
Elad Cohen <ecohen@cloudbees.com>
2023-07-31 13:03:15 +0300

    wip

--- apps/playground/scripts/dev.ts
+++ apps/playground/scripts/dev.ts
@@ -6,9 +6,12 @@ import { join } from "path";
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const wingfile = join(__dirname, "../dist/test.w");
+const packagefile = join(__dirname, "../dist/package.json");
 
 try {
   await writeFile(wingfile, "", "utf-8");
+  // https://github.com/winglang/wing/issues/3601
+  await writeFile(packagefile, "{}", "utf-8");
   const port = await startConsole({ wingfile });
   process.env["VITE_CONSOLE_SERVER_URL"] = `http://localhost:${port}`;
   const vite = await createServer();
