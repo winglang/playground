624fe9ead9caa82fb96fb50db28626b20f1faf1e
Elad Cohen <eladc@monada.co>
2023-08-06 15:33:22 +0300

    chore: remove webcontainers redirect

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -5,20 +5,11 @@ import App from "./App";
 import "@wing-playground/shared/src/index.css";
 import { loadWASM } from "onigasm";
 import wasmUrl from "@wing-playground/shared/src/onigasm.wasm?url";
-import { maybeRedirect } from "@wing-playground/shared/src/maybe-redirect";
 
-const redirected = await maybeRedirect({
-  maxTabs: 3,
-  mainSite: import.meta.env.VITE_MAIN_SITE_URL,
-  subdomainSite: import.meta.env.VITE_SUBDOMAIN_SITE_URL,
+loadWASM(wasmUrl).then(() => {
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
 });
-
-if (!redirected) {
-  loadWASM(wasmUrl).then(() => {
-    ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    );
-  });
-}
--- apps/tour/src/vite-env.d.ts
+++ apps/tour/src/vite-env.d.ts
@@ -5,14 +5,6 @@ interface ImportMetaEnv {
    * Time in seconds to wait before showing a warning for slowness.
    */
   readonly VITE_TOO_SLOW_ERROR_SECONDS_THRESHOLD?: number;
-  /**
-   * The URL of the main site.
-   */
-  readonly VITE_MAIN_SITE_URL?: string | undefined;
-  /**
-   * The URL of the subdomain site.
-   */
-  readonly VITE_SUBDOMAIN_SITE_URL?: string | undefined;
 }
 
 interface ImportMeta {
