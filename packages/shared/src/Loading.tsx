f4c09ddb911634811fc78d44853d182779c2b2d2
polamoros <info@polamoros.com>
2024-03-01 11:07:28 +0100

    wip

--- packages/shared/src/Loading.tsx
+++ packages/shared/src/Loading.tsx
@@ -1,23 +1,27 @@
 import { FiCommand } from "react-icons/fi";
 import classNames from "classnames";
-import WingLogo from "./wing_logo.svg";
+import { WingLogo } from "./WingLogo";
 
 export interface LoadingProps {
   status: string;
 }
 
 export function Loading({ status }: LoadingProps) {
-  return <div className={classNames(
-    "flex flex-col h-full w-full justify-start items-center",
-    "text-slate-500 dark:text-[#f1f0f1]",
-    "transition-colors duration-300",
-  )}>
-    <div className="mt-[20%]">
-      <img className="h-[64px] w-[136px]" src={WingLogo} loading="lazy" alt="" />
+  return (
+    <div
+      className={classNames(
+        "flex flex-col",
+        "h-full justify-center items-center",
+        "text-slate-500 dark:text-[#f1f0f1]",
+        "transition-colors duration-300",
+        "space-y-4",
+      )}
+    >
+      <WingLogo className="h-[40px]" />
+      <div className="flex items-center">
+        <FiCommand className="animate-spin" />
+        <span className="pl-[5px]">{status}</span>
+      </div>
     </div>
-    <div className="flex items-center">
-      <FiCommand className="animate-spin"/>
-      <span className="pl-[5px]">{status}</span>
-    </div>
-  </div>
+  );
 }
