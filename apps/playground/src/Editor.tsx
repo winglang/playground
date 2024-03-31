f63e992e49e2a80f445e9aa71c693af531df2711
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2024-03-31 11:49:55 +0300

    non absolute

--- apps/playground/src/Editor.tsx
+++ apps/playground/src/Editor.tsx
@@ -365,9 +365,7 @@ export const ReactMonacoEditor: React.FC<EditorProps> = ({}) => {
                     "grow h-full basis-auto border",
                     theme.border4,
                     "transition-colors duration-300",
-                    isFullScreen
-                      ? "absolute w-full h-full top-2 right-0 border-2"
-                      : "",
+                    isFullScreen ? "w-full h-full top-2 right-0 border-2" : "",
                   )}
                 >
                   {loadingStatus !== LoadingStatus.Completed && (
