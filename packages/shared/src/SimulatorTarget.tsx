e01f8791e527db406494d540a4e1ab41a1401559
Tsuf Cohen <39455181+tsuf239@users.noreply.github.com>
2024-04-02 17:26:00 +0300

    control layout via searchParams (#851)
    - the layout of the playground console can be controlled via a search
    param now (`layout`)
    - fixed a typo
    - allowing display of the console on full screen depends on URL search
    param (`full`)
    
    
    https://www.loom.com/share/b32805e74992432aadae5b73c8825a7a?sid=945d4f0e-8041-4c50-8e6e-0e2aa3c677b8

--- packages/shared/src/SimulatorTarget.tsx
+++ packages/shared/src/SimulatorTarget.tsx
@@ -2,19 +2,19 @@ import { MutableRefObject } from "react";
 
 export interface SimulatorTargetProps {
   frameSrc: string;
-  iframeRef: MutableRefObject<any>
+  iframeRef: MutableRefObject<any>;
 }
 
 export const SimulatorTarget = (props: SimulatorTargetProps) => {
   return (
     <div className="h-full">
       <iframe
-        id='console'
-        className='basis-auto w-full h-full'
+        id="console"
+        className="basis-auto w-full h-full"
         src={props.frameSrc}
         allowFullScreen={true}
         ref={props.iframeRef}
       ></iframe>
     </div>
-  )
-}
+  );
+};
