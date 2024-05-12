d17b6c130e9b405aa15c5f3a5b9d70e199a2d6a1
wingbot <109207340+monadabot@users.noreply.github.com>
2024-05-12 08:04:21 -0400

    feat: update wing version (#1140)
    feat: update wing version
    Updates the Wing version. See details in [workflow run].
    [Workflow Run]:
    https://github.com/winglang/playground/actions/runs/9050872873
    ------
    *Automatically created via the "update-wing" workflow*
    
    ---------
    
    Signed-off-by: monabot <monabot@monada.co>
    Co-authored-by: Elad Cohen <eladc@monada.co>

--- apps/playground/tests/helper.ts
+++ apps/playground/tests/helper.ts
@@ -2,7 +2,7 @@ import {Page} from "@playwright/test";
 
 export const getResourceNodeFromConsoleMap = (page: Page, path: string) => {
     return page.frameLocator('#console')
-        .locator(`[data-testid=map-pane] [data-testid='map-node:${path}']`);
+        .locator(`[data-testid=map-pane] [data-elk-id='${path}']`);
 }
 
 export const getResourceFromConsoleInteraction = (page: Page, dataTestId: string) => {
