bbe758c5382e7564f8600bf35785215e906efc9c
Elad Cohen <eladc@monada.co>
2024-05-15 14:25:03 +0300

    wip

--- packages/shared/src/editor/use-editor.tsx
+++ packages/shared/src/editor/use-editor.tsx
@@ -88,11 +88,11 @@ export const useEditor = ({
         editorRef.current?.setValue(code);
 
         try {
-          onLoadingStatusChange(LoadingStatus.Completed);
+          onLoadingStatusChange(LoadingStatus.Eval);
           const { uiUrl, updateUrl } = await createConsole(layout);
           consoleRef.current = updateUrl;
-          setIframeSrc(uiUrl)
           await evaluateCode(true);
+          setIframeSrc(uiUrl)
           startLsp({ onError: onLspError});
         } catch (err) {
           console.error("installing server console", err);
